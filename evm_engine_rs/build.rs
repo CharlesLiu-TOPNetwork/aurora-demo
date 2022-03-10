59fd655e3d04314acd7cd4002f174790a05b48db
CharlesLiu <charles.liu@upblocks.io>
2022-03-10 14:33:09 +0800

    dev(cmake): deploy_code test with cmake ok

---
+++ evm_engine_rs/build.rs
@@ -0,0 +1,33 @@
+#![allow(unused)]
+use std::env;
+
+use std::process::Command;
+
+#[cfg(feature = "build_as_xtop_lib")]
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // Command::new("cp")
+    //     .arg(&format!("/tmp/test.txt"))
+    //     .arg(&format!("{}", out_dir))
+    //     .status()
+    //     .unwrap();
+
+    // from: target/release/build/xevm_engine-e40834640f8d4de0/out/
+    // to :|-  evm_engine_rs/debug/...
+    //     |-  lib/linux/*.a
+    Command::new("cp")
+        .arg(&format!(
+            "{}/../../../../../lib/Linux/libxtop_mock_api.a",
+            out_dir
+        ))
+        .arg(&format!("{}", out_dir))
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=xtop_mock_api");
+}
+
+#[cfg(not(feature = "build_as_xtop_lib"))]
+fn main() {}
--- evm_engine_rs/engine/src/interface.rs
+++ evm_engine_rs/engine/src/interface.rs
@@ -1,10 +1,40 @@
 #![allow(unused)]
 
+use crate::prelude::*;
+
 mod interface {
+    use crate::engine::Engine;
     use crate::prelude::*;
+    use engine_sdk::{
+        env::Env,
+        io::{StorageIntermediate, IO},
+        runtime::Runtime,
+    };
+
+    fn predecessor_address(predecessor_account_id: &AccountId) -> Address {
+        top_account_to_evm_address(predecessor_account_id.as_bytes())
+    }
 
     #[no_mangle]
-    pub extern "C" fn deploy_code() {}
+    pub extern "C" fn deploy_code() {
+        let io = Runtime;
+        let input = io.read_input().to_vec();
+        let current_account_id = io.current_account_id();
+        let mut engine = Engine::new(
+            predecessor_address(&io.predecessor_account_id()),
+            current_account_id,
+            io,
+            &io,
+        )
+        .sdk_unwrap();
+        Engine::deploy_code_with_input(&mut engine, input)
+            .map(|res| {
+                println!("res: {:?}", res);
+                res.try_to_vec().sdk_expect("ERR_SERIALIZE")
+            })
+            .sdk_process();
+        // TODO: charge for storage
+    }
     #[no_mangle]
     pub extern "C" fn call_contract() {}
 }
