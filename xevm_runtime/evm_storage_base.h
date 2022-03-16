42ab1e980cd8a3d8f9f66edd84c8773e967cd405
CharlesLiu <charles.liu@upblocks.io>
2022-03-16 13:59:51 +0800

    add mock evm storage

--- xevm_runtime/evm_storage_base.h
+++ xevm_runtime/evm_storage_base.h
@@ -1,11 +1,14 @@
 #pragma once
+#include "assert.h"
 #include "xevm_runtime/evm_storage_face.h"
+
+#include <string>
 
 namespace top {
 namespace evm {
 
-enum class ext_type {
-    Config = 0x0,
+enum class storage_key_type {
+    // Config = 0x0,
     Nonce = 0x1,
     Balance = 0x2,
     Code = 0x3,
@@ -13,6 +16,11 @@ enum class ext_type {
     Generation = 0x5,
 
     ALL = 0x6  // show all
+};
+
+struct storage_key {
+    storage_key_type key_type;
+    std::string address;  // todo use T6?...
 };
 
 class xtop_evm_storage_base : public xevm_storage_face_t {
@@ -24,9 +32,26 @@ public:
     xtop_evm_storage_base & operator=(xtop_evm_storage_base &&) = default;
     ~xtop_evm_storage_base() override = default;
 
-public:
+protected:
     // todo might add some utl function here to decode key type.
+
+    /// [1 byte]   -   [1 byte]   - [20 bytes]
+    //  [version]  - [key perfix] -  [address]
+    storage_key decode_key_type(bytes const & key) {
+        storage_key res;
+        // assert(key.size() == 22);
+        res.key_type = storage_key_type(key[1]);
+        res.address.resize(2 * (key.size() - 2));
+        // eth address 20 hex
+        static constexpr char hex[] = "0123456789abcdef";
+        for (std::size_t index = 0; index < key.size() - 2; ++index) {
+            res.address[2 * index] = hex[key[index + 2] / 16];
+            res.address[2 * index + 1] = hex[key[index + 2] % 16];
+        }
+        return res;
+    }
 };
+
 using xevm_storage_base_t = xtop_evm_storage_base;
 }  // namespace evm
 }  // namespace top
