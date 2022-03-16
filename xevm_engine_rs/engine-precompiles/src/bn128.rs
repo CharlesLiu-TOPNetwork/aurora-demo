07344f4c9e0dc8fc0b49fb2147ed7ffaa93c1eaf
LonDong <lon.dong@topnetwork.org>
2022-03-16 02:53:47 -0700

    add top blake2

--- xevm_engine_rs/engine-precompiles/src/bn128.rs
+++ xevm_engine_rs/engine-precompiles/src/bn128.rs
@@ -304,19 +304,16 @@ impl Precompile for Bn128Pair<Berlin> {
 }
 
 #[cfg(test)]
-pub fn new_context() -> Context {
-    Context {
-        address: Default::default(),
-        caller: Default::default(),
-        apparent_value: Default::default(),
-    }
-}
-
-#[cfg(test)]
 mod tests {
-    // use crate::utils::new_context;
-
     use super::*;
+
+    pub fn new_context() -> Context {
+        Context {
+            address: Default::default(),
+            caller: Default::default(),
+            apparent_value: Default::default(),
+        }
+    }
 
     #[test]
     fn test_alt_bn128_add() {
